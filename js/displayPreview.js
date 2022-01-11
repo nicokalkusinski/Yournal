f2fd0d9be2c955a30dc9cb8b156695776d845e1c
raitrow <raitrow@users.noreply.github.com>
2022-01-11 13:14:47 +0100

    adjustments with css

--- js/displayPreview.js
+++ js/displayPreview.js
@@ -2,19 +2,22 @@ function reloadList() {
   let items = document.getElementsByClassName("list-item");
   let emptyListText = document.getElementById("list-empty");
   let listLegend = document.getElementById("list-legend");
+  let contentLegend = document.getElementById("content-legend");
   if(items.length == 0) {
     emptyListText.style.display = "block";
     listLegend.style.display = "none";
+    contentLegend.style.display = "none";
   } else {
     emptyListText.style.display = "none";
     listLegend.style.display = "grid";
+    contentLegend.style.display = "grid";
   }
 }
 reloadList();
 
 function displayPreview(id) {
   //preview appearance
-  let preview = document.getElementById("content-preview");
+  let preview = document.getElementById("note-preview");
   let nav_bar = document.getElementsByTagName("nav")[0];
   preview.style.opacity = 1;
   preview.style.transform = "TranslateY(0rem)";
@@ -40,7 +43,7 @@ function displayPreview(id) {
 }
 
 function noPreview() {
-  let preview = document.getElementById("content-preview");
+  let preview = document.getElementById("note-preview");
   preview.style.opacity = 0;
   preview.style.transform = "TranslateY(1rem)";
 }
